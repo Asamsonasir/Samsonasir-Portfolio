e1f0d4898e2d4e58f71b35c9b474695e6d33a938
Asamsonasir <samsonasir0602@gmail.com>
2025-05-29 03:44:41 +0530

    Sam Portfolio

---
+++ script.js
@@ -0,0 +1,14 @@
+
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+      }
+    });
+  }, {
+    threshold: 0.3
+  });
+
+  const aboutSection = document.querySelector('.about');
+  observer.observe(aboutSection);
+
